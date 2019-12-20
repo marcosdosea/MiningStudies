91f644f7dc866a67645b52bf4077b7febd85527e
marcosdosea <marcosdosea@yahoo.com.br>
2019-12-19 22:37:38 -0300

    New studies including the smell long class based on metric LOC of class.

--- src/study/smell/FilterSmellSimple.java
+++ src/study/smell/FilterSmellSimple.java
@@ -18,7 +18,7 @@ public class FilterSmellSimple {
 
 		System.out.println("Iniciando a coleta de métricas do projeto a ser analisado...");
 		ArrayList<String> projetosAnalisar = executor.lerProjetos("AnalysisSimple.txt");
-		MetricReport metricasProjetosAnalisar = executor.getMetricsFromProjects(projetosAnalisar, PASTA_RESULTADO, "");
+		MetricReport metricasProjetosAnalisar = executor.getMetricsFromProjects(projetosAnalisar, PASTA_RESULTADO, "efdc59004cd254692aa749fab3b1c35aa6680a9d");
 
 		System.out.println("Gerando DR.csv com a lista classes e design roles atribuídos...");
 		executor.saveDesignRoles(metricasProjetosAnalisar.all(), System.getProperty("user.dir") + "\\results\\DR.CSV");
@@ -34,5 +34,6 @@ public class FilterSmellSimple {
 		System.out.println("Gerando SMELLS.csv com a lista de problemas de design encontrados...");
 		FilterSmellResult result = FilterSmells.filtrar(metricasProjetosAnalisar.all(), listaTecnicas, "HEAD");
 		FilterSmells.gravarMetodosSmellProgramador(result.getMetodosSmell(), System.getProperty("user.dir") + "\\results\\SMELLS.csv");
+		FilterSmells.gravarClassesSmellProgramador(result.getClassesSmell(), System.getProperty("user.dir") + "\\results\\SMELLS-classes.csv");
 	}
 }
