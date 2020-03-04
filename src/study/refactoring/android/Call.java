9fde2b2aae953bbc2cfee176cd57b3485da97b5c
marcosdosea <marcosdosea@yahoo.com.br>
2020-03-03 23:23:25 -0300

    metric thresholds Web and Android

---
+++ src/study/refactoring/android/Call.java
@@ -0,0 +1,13 @@
+package study.refactoring.android;
+
+public class Call {
+
+	public static void main(String[] args) {
+		AmazeFileManagerStudy.main(args);
+		TransdroidStudy.main(args);
+		BitcoinStudy.main(args);
+		K9Study.main(args);
+		SignalStudy.main(args);
+		FairEmailStudy.main(args);
+	}			
+}
